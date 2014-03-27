e5fbe459ebb3d4b1ffe3a9f6bb045c41d0125cf6
tom buckley <buckley.tom@gmail.com>
2014-03-27 10:34:02 -0700

    completed get councilmember story - looks ugly

--- js/main.js
+++ js/main.js
@@ -5,9 +5,13 @@
 
   document.getElementById("ajaxButton").onclick = function() { 
     var address = document.getElementById("ajaxTextbox").value;
-    makeRequest('https://www.googleapis.com/civicinfo/us_v1/representatives/lookup?key=AIzaSyAzOF-EGWefl40wZ28RNwZLG4MRVQCw6cg',address); };
+    makeRequest(
+      'https://www.googleapis.com/civicinfo/us_v1/representatives/lookup?key=AIzaSyAzOF-EGWefl40wZ28RNwZLG4MRVQCw6cg',
+      address, 
+      sendToDOM); 
+  };
 
-  function makeRequest(url,address) {
+  function makeRequest(url,address,callback) {
     if (window.XMLHttpRequest) { // Mozilla, Safari, ...
       httpRequest = new XMLHttpRequest();
     } else if (window.ActiveXObject) { // IE
@@ -26,20 +30,38 @@
       alert('Giving up :( Cannot create an XMLHTTP instance');
       return false;
     }
-    httpRequest.onreadystatechange = alertContents;
+    httpRequest.onreadystatechange = callback;
     httpRequest.open('POST', url);
     httpRequest.setRequestHeader('Content-Type','application/json;charset=UTF-8');
     var jsonparams = JSON.stringify({"address":address});
     httpRequest.send(jsonparams);
   }
 
-  function alertContents() {
+  function sendToDOM() {
     if (httpRequest.readyState === 4) {
       if (httpRequest.status === 200) {
-        var response = JSON.parse(httpRequest.responseText);
-        console.log(response);
+        var repdata = JSON.parse(httpRequest.responseText);
+        console.log(repdata);
+          if (repdata.officials.P28.channels) {
+            var result = '<div class="col-lg-4">\
+            <h2>' + repdata.officials.P28.name + '</h2>\
+            <img src=' + repdata.officials.P28.photoUrl + '>\
+            <p><a href="http://www.twitter.com/' + repdata.officials.P28.channels[1].id.replace(" ", "") + '\
+            ">http://www.twitter.com/\
+            ' + repdata.officials.P28.channels[1].id + '</a></p>\
+            </div>';
+          }
+          else
+          {
+            var result = '<div class="col-lg-4">\
+            <h2>' + repdata.officials.P28.name + '</h2>\
+            <img src=' + repdata.officials.P28.photoUrl + '></p>\
+            </div>';
+          } 
+
+
+        $("#councilmember").replaceWith(result);
       } else {
-      console.log(httpRequest.responseText)
       alert('There was a problem with the request.');
       }
     }
